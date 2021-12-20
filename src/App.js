4c6ed160c8c9ea1336c3baf93121e8e2852a9a26
Josh <zylo.codes@gmail.com>
2021-12-20 16:57:49 -0600

    starter code

--- src/App.js
+++ src/App.js
@@ -1,115 +1,16 @@
-import { useState } from 'react'
-import { FaSkull, FaTrophy } from 'react-icons/fa';
-import Controls from './components/controls';
 import Header from './components/header';
-import Player from './components/player'
-import { getComputerChoice } from './utils/helpers'
 import './App.css';
 
-const defaultMessage = 'CHOOSE'
-
-const defaultPlayers = {
-  human: {
-    choice: null,
-    lives: 3,
-  },
-  computer: {
-    choice: null,
-    lives: 3,
-  }
-}
-
 function App() {
-  const [players, setPlayers] = useState(defaultPlayers)
-  const [message, setMessage] = useState(defaultMessage)
-
-  const handleChoiceChange = huChoice => {
-    const { human, computer } = players
-    const compChoice = getComputerChoice()
-    let message
-    if (
-      (huChoice == "r" && compChoice == "s") || 
-      (huChoice == "p" && compChoice == "r") || 
-      (huChoice == "s" && compChoice == "p")
-    ) {
-      computer.lives--
-      message = 'ROUND WON'
-    } else if (huChoice === compChoice) {
-      message = 'TIE'
-    } else {
-      human.lives--
-      message = 'ROUND LOST'
-    }
-    setMessage(message)
-    setPlayers({
-      human: {
-        ...human,
-        choice: huChoice
-      },
-      computer: {
-        ...computer,
-        choice: compChoice
-      },
-    })
-  }
-
-  const handleNextRound = () => {
-    setPlayers({
-      human: {
-        ...players.human,
-        choice: null,
-      },
-      computer: {
-        ...players.computer,
-        choice: null,
-      }
-    })
-    setMessage(defaultMessage)
-  }
-
-  const reset = () => {
-    setPlayers(defaultPlayers)
-    setMessage(defaultMessage)
-  }
-
-  return (players.human.lives === 0 || players.computer.lives === 0) ? (
-    <div className='game-over'>
-      {players.human.lives === 0 ? (
-        <>
-          <FaSkull size={100} />
-          <h1>YOU LOSE</h1>
-        </>
-      ) : (
-        <>
-          <FaTrophy size={100} />
-          <h1>YOU WON!</h1>
-        </>
-      ) }
-      <button onClick={reset}>Play Again</button>
-    </div>
-  ) : (
+  return (
     <>
       <Header />
 
       <div className="player-container">
-        <Player 
-          name="human"
-          choice={players.human.choice}
-          lives={players.human.lives}
-        />
-        <Player 
-          name="computer"
-          choice={players.computer.choice}
-          lives={players.computer.lives}
-        />
+        {/* TODO: Player components */}
       </div>
       
-      <Controls 
-        selected={players.human.choice}
-        handleChoiceChange={handleChoiceChange}
-        message={message}
-        handleNextRound={handleNextRound}
-      />
+      {/* TODO: Controls component */}
     </>
   )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,10 @@
 import Logo from '../logo.svg'
 
-const Header = () => <h1 style={{ display: "flex", justifyContent: 'center', alignItems: 'center' }}>
-  <img src={Logo} width="150" /> Rock Paper Scissors
-</h1>
+const Header = () => (
+  <header className='app-header'>
+    <img src={Logo} width="150" /> 
+    <h1>Rock Paper Scissors</h1>
+  </header>
+)
 
 export default Header
