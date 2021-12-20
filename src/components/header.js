a0ad032f1f49992615a0e9963cc688bb090cd7a9
Josh <zylo.codes@gmail.com>
2021-12-20 17:03:52 -0600

    adds react logo spin animtion

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import Logo from '../logo.svg'
 
 const Header = () => (
   <header className='app-header'>
-    <img src={Logo} width="150" /> 
+    <img src={Logo} className='app-logo' width="150" /> 
     <h1>Rock Paper Scissors</h1>
   </header>
 )
